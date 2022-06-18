c7b3229285b3339826996764e43febd513b2dde5
anant <anantkumar312@gmail.com>
2022-06-18 21:14:34 +0530

    Corrected strings

--- footer.php
+++ footer.php
@@ -10,17 +10,17 @@
           <h6 class="text-uppercase fw-bold mb-4">
             <i class="fas fa-gem me-3"></i>Bakery
           </h6>
-          <p>
-            we provide you a trustworthly and covenient platform to choose best cake for your family,friends etc.for every occassion like birthdays anniversaries and many more.we offer wide range of products in various categories eggless cakes,choclates etc.
+          <p><i class="fas fa-home me-3">
+              We provide you a trustworthly and covenient platform to choose best cake for your family, friends and other loved once for every occassion like birthdays, anniversaries and many more. We offer wide range of products in various categories eggless cakes, choclate cakes, etc.
           </p>
         </div>
         <h6 class="text-uppercase fw-bold mb-4">
           Contact
         </h6>
-        <p><i class="fas fa-home me-3"></i>India</p>
+        <p><i class="fas fa-home me-3"></i>College Road, Nashik, Maharashtra, India</p>
         <p>
           <i class="fas fa-envelope me-3"></i>
-          info@gmail.com
+          bakersvilla@gmail.com
         </p>
         <p><i class="fas fa-phone me-3"></i> + 91 9511649569</p>
         <p><i class="fas fa-print me-3"></i> + 91 7391867951</p>
@@ -31,7 +31,7 @@
   <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
     <div class="p-3 mb-2 bg-secondary bg-gradient text-white">
       © 2022 Copyright:
-      <a class="text-reset fw-bold" href="#">Bakery shop</a>
+      <a class="text-reset fw-bold" href="#">Bakersvilla</a>
     </div>
   </div>
 </footer>
