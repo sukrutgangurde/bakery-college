7f6cee1a57bc3d5f41181b9de17fe6d5b8ea9da8
anant <anantkumar312@gmail.com>
2022-06-19 16:34:48 +0530

    renamed newfooter to footer

--- footer.php
+++ footer.php
@@ -1,35 +1,34 @@
-<footer class="text-center text-lg-start bg-light text-muted">
-  <section class="">
-    <div class="p-3 mb-2 bg-secondary bg-gradient text-white">
-      <div class="container text-center text-md-start mt-5">
-        <div class="row mt-3">
-          <h6 class="text-uppercase fw-bold mb-4">
-            <i class="fas fa-gem me-3"></i>Bakery
-          </h6>
-          <p><i class="fas fa-home me-3">
-              We provide you a trustworthly and covenient platform to choose best cake for your family, friends and other loved once for every occassion like birthdays, anniversaries and many more. We offer wide range of products in various categories eggless cakes, choclate cakes, etc.
-          </p>
+<footer class="footer" id="footer">
+    <div class="container">
+        <div class="footer__container">
+            <div class="footer__container__main">
+                <div class="footer__container__main__logo">
+                    <img src="proimages/logo.png" alt="BakersVilla Logo">
+                </div>
+                <div class="footer__container__main__address">
+                    <p>College Road, Nashik, Maharashtra, India</p>
+                    <p>bakersvilla@gmail.com</p>
+                    <p>+91 9511649569</p>
+                    <p>+91 7391867951</p>
+                </div>
+            </div>
+            <div class="footer__container__links-div">
+                <div class="footer__container__links-div__sect">
+                    <p class="footer__container__links-div__sect__title">Social Handles</p>
+                    <a href="#" class="footer__container__links-div__sect__link">Email
+                    </a>
+                    <a href="#" class="footer__container__links-div__sect__link">Linkedin
+                    </a>
+                    <a href="#" class="footer__container__links-div__sect__link">Instagram
+                    </a>
+                    <a href="#" class="footer__container__links-div__sect__link">Twitter
+                    </a>
+                </div>
+            </div>
+            <div class="footer__container__para">
+                We provide you a trustworthly and covenient platform to choose best cake for your family, friends and other loved once for every occassion like birthdays, anniversaries and many more. We offer wide range of products in various categories eggless cakes, choclate cakes, etc.
+            </div>
         </div>
-        <h6 class="text-uppercase fw-bold mb-4">
-          Contact
-        </h6>
-        <p><i class="fas fa-home me-3"></i>College Road, Nashik, Maharashtra, India</p>
-        <p>
-          <i class="fas fa-envelope me-3"></i>
-          bakersvilla@gmail.com
-        </p>
-        <p><i class="fas fa-phone me-3"></i> + 91 9511649569</p>
-        <p><i class="fas fa-print me-3"></i> + 91 7391867951</p>
-      </div>
     </div>
-    </div>
-  </section>
-  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
-    <div class="p-3 mb-2 bg-secondary bg-gradient text-white">
-      © 2022 Copyright:
-      <a class="text-reset fw-bold" href="#">Bakersvilla</a>
-    </div>
-  </div>
+    <div class="footer__copyright">Copyright © 2022 BakersVilla</div>
 </footer>
-<script type="text/javascript" src="js/mdb.min.js"></script>
-<script type="text/javascript"></script>
