2694bb4d2d1aab6b44c127a027693724cd0c85f9
Anant Dubey <93765660+dubeyanant@users.noreply.github.com>
2022-06-19 23:46:38 +0530

    redesigned home page (#3)
    Co-authored-by: Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>

--- head.php
+++ head.php
@@ -4,3 +4,5 @@ pg_connect("host=127.0.0.1 user=postgres password=postgres dbname=bakery");
 ?>
 <link rel="stylesheet" href="css/mdb.min.css" />
 <link rel="stylesheet" href="css/footer.css" />
+<link rel="stylesheet" href="css/swiper-bundle.min.css" />
+<link rel="stylesheet" href="css/index.css" />
