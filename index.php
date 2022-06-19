c80ce64feb03fc53bb2b9e217654068067ebaa15
Anant Dubey <93765660+dubeyanant@users.noreply.github.com>
2022-06-20 00:03:57 +0530

    Minor changes to index.php (#4)
    * redesigned home page
    
    * minor changes
    
    Co-authored-by: Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 <html>
 
 <head>
-  <?php include 'head.php'; ?>
-  <title></title>
+    <?php include 'head.php'; ?>
+    <title></title>
 </head>
 
 <body>
   <?php include 'menu.php'; ?>
-
   <header class="header" style="color:black;">
+
         <div class="container">
             <div class="header__text">
                 <div class="header__text__logo">
@@ -32,9 +32,12 @@
         <!-- Additional required wrapper -->
         <div class="swiper-wrapper">
             <!-- Slides -->
-            <div class="swiper-slide"><img class="swiper-img" src="./proimages/bread.jpg"></div>
-            <div class="swiper-slide"><img class="swiper-img" src="./proimages/bread.jpg"></div>
-            <div class="swiper-slide"><img class="swiper-img" src="./proimages/bread.jpg"></div>
+            <div class="swiper-slide"><img class="swiper-img" src="./proimages/cake5.jpg"></div>
+            <div class="swiper-slide"><img class="swiper-img" src="./proimages/cake6.jpg"></div>
+            <div class="swiper-slide"><img class="swiper-img" src="./proimages/cookie4.jpg"></div>
+            <div class="swiper-slide"><img class="swiper-img" src="./proimages/cookie5.jpg"></div>
+            <div class="swiper-slide"><img class="swiper-img" src="./proimages/bread2.jpg"></div>
+            <div class="swiper-slide"><img class="swiper-img" src="./proimages/pastry1.jpg"></div>
             ...
         </div>
         <!-- If we need pagination -->
@@ -43,46 +46,10 @@
         <!-- If we need navigation buttons -->
         <div class="swiper-button-prev"></div>
         <div class="swiper-button-next"></div>
-
-    </div>
-    <div class="about" style="color:black;">
-        <div class="container">
-            <div class="about__text">
-                <h2 class="about__text__title">About Us</h2>
-                <p class="about__text__content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
-                    dolorum praesentium at recusandae nemo ducimus delectus amet consectetur similique quia
-                    voluptatem,
-                    numquam totam quos veritatis laborum molestias, fuga quis dolores corrupti eaque in atque. Illo
-                    excepturi, ab impedit dicta reiciendis ex dolore laudantium tempora odit, expedita quisquam
-                    saepe,
-                    nemo optio animi aperiam harum voluptas nihil maiores totam adipisci. Obcaecati voluptates
-                    quidem
-                    placeat sint tempore! Quas doloribus dolorum cum commodi, tempore nemo minus omnis quod qui
-                    porro
-                    id? Ratione perferendis voluptas dolore deserunt, iure amet modi corporis hic expedita
-                    reprehenderit
-                    repellat nesciunt sed sint minus facilis quidem accusantium, tempora veniam accusamus!</p>
-            </div>
-            <div class="about__images">
-                <div class="about__images__img-div">
-                    <img src="./proimages/cream1.jpg" alt="#">
-                </div>
-                <div class="about__images__img-div">
-                    <img src="./proimages/cream1.jpg" alt="#">
-                </div>
-                <div class="about__images__img-div">
-                    <img src="./proimages/cream1.jpg" alt="#">
-                </div>
-                <div class="about__images__img-div">
-                    <img src="./proimages/cream1.jpg" alt="#">
-                </div>
-            </div>
-        </div>
     </div>
 
     <script src="./js/swiper-bundle.min.js"></script>
     <script>
-
         var swiper = new Swiper(".swiper", {
             grabCursor: true,
             effect: "creative",
@@ -113,8 +80,7 @@
             }
         });
     </script>
-  
-  <?php include 'footer.php'; ?>
+    <?php include 'footer.php'; ?>
 </body>
 
 </html>
