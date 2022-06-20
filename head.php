79d3ca7da27853611bdc5d4e1eebc75f870f0cce
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2022-06-20 13:55:23 +0530

    redesigned Menu bar

--- head.php
+++ head.php
@@ -7,3 +7,4 @@ pg_connect("host=127.0.0.1 user=postgres password=postgres dbname=bakery");
 <link rel="stylesheet" href="css/footer.css" />
 <link rel="stylesheet" href="css/swiper-bundle.min.css" />
 <link rel="stylesheet" href="css/index.css" />
+<link rel="stylesheet" href="css/nav.css" />
